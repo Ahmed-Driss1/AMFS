eca5453086be730ec106647653bd1c8fe2881d77
Ahmed-Driss1 <drissiahmed.acc@gmail.com>
2024-11-16 00:22:04 +0100

    added font new hero
    went with a whole new hero section design

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,16 +1,15 @@
 export default function Hero() {
     return (
         <>
-            <section className="w-full min-h-screen bg-light flex flex-col items-center justify-center gap-y-24">
-                <div className="flex flex-col justify-center "> 
-                <h1 className="mt-10 sm:mt-10 md:mt-20 font-amulya text-5xl sm:text-4xl md:text-5xl lg:text-7xl text-center text-accent">
-                    Come surf with us!
+            <section className="min-h-screen bg-light  ">
+                
+                <h1 className="font-Shrikhand text-accent lg:text-8xl sm:text-6xl text-4xl text-left pb-20 lg:pl-10 sm:pl-7  absolute bottom-0 w-full max-w-full ">
+                Ride waves, <br /> create memories.
                 </h1>
-                <p className="text-dark font-synonym text-center text-sm">some rondom texte I didn't decide it yet ok</p>
-                </div>
-                <button className="font-synonym font-black  border-4 border-accent text-dark lg:text-2xl lg:h-20 sm:w-1/2 sm:text-lg md:w-1/3 lg:w-1/5 p-3 rounded-lg lg:hover:scale-110 transition-transform duration-500 mt-6">
-                    Book a Session
-                </button>
+                
+                
+
+                
             </section>
         </>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,18 @@ export default {
       fontFamily: {
         synonym: ['Synonym', 'sans-serif'],
         amulya: ['Amulya', 'sans-serif'],
+        Shrikhand: ['Shrikhland', 'serif'],
       },
       colors: {
         light: '#ECE3DE',
         accent: '#1A548A',
         dark: '#2F2C2C',
+      },
+      screens: {
+        sm: '640px',  // Applies for screens >= 640px
+        md: '768px',  // Applies for screens >= 768px
+        lg: '1024px', // Applies for screens >= 1024px
+        xl: '1280px', // Applies for screens >= 1280px
       },
     },
   },
